2aa4e34eae21b1b460e5e43a4554d1dd2788879a
xiaonan song <4king-gruuby@163.com>
2013-01-24 17:11:42 +0800

    fix module

--- lib/Asthma/Spider/JingDongMobile.pm
+++ lib/Asthma/Spider/JingDongMobile.pm
@@ -1,16 +1,11 @@
 package Asthma::Spider::JingDongMobile;
 use Moose;
 extends 'Asthma::Spider';
-with 'Asthma::Tool';
 
 use utf8;
 use Asthma::Item;
 use Asthma::Debug;
 use HTML::TreeBuilder;
-use AnyEvent;
-use AnyEvent::HTTP;
-use HTTP::Headers;
-use HTTP::Message;
 use URI;
 #use Data::Dumper;
 
@@ -29,7 +24,6 @@ sub run {
 
     my $run = 1;
     while ( $run ) {
-	#debug("self urls" . Dumper $self->urls);
 	if ( my $url  = shift @{$self->urls} ) {
 	    my $resp = $self->ua->get($url);
 	    $self->find($resp);
@@ -43,12 +37,10 @@ sub find {
     my $self = shift;
     my $resp = shift;
     return unless $resp;
-    
+
     debug("process: " . $resp->request->uri->as_string);
 
-    my @skus;
-
-    if ( my $content = $resp->decoded_content ) {
+    if ( my $content = $resp->decoded_content )  {
         my $tree = HTML::TreeBuilder->new_from_content($content);
         
         # find next page
@@ -62,91 +54,71 @@ sub find {
             }
         }
 
-        # find skus
-        foreach my $plist ( $tree->look_down("id", "plist") ) {
-            foreach my $item ( $plist->look_down(_tag => 'li', sku => qr/\d+/) ) {
-                push @skus, $item->attr("sku");
+        # parse list page
+        my @chunks;
+        if ( my $plist = $tree->look_down('id', 'plist') ) {
+            @chunks = $plist->look_down(_tag => 'li', sku => qr/\d+/)
+        }
+
+        foreach my $c ( @chunks ) {
+            my $item = Asthma::Item->new();
+            if ( my $p = $c->look_down(class => "p-name") ) {
+                if ( my $title = $p->as_trimmed_text ) {
+                    $item->title($title);
+                }
             }
+
+            if ( my $sku = $c->attr("sku") ) {
+                $item->sku($sku);
+                $item->url("http://www.360buy.com/product/" . $sku . ".html");
+                if ( my $price = $self->fprice($sku) ) {
+                    $item->price($price);
+                }
+            }
+
+            if ( my $p = $c->look_down(class => "p-img") ) {
+                if ( my $img = $p->look_down(_tag => "img") ) {
+                    if ( my $image_url = $img->attr("src") || $img->attr("src2") ) {
+                        $item->image_url($image_url);
+                    }
+                }
+            }
+
+            my $con = $c->as_trimmed_text;
+	    if ( $con =~ m{到货通知} ) {
+                $item->available('out of stock');
+	    }
+
+            debug_item($item);
+            $self->add_item($item);
         }
 
         $tree->delete;
     }
-
-    if ( @skus ) {
-	my $cv = AnyEvent->condvar;
-
-	foreach my $sku ( @skus ) {
-            my $url = "http://www.360buy.com/product/$sku.html";
-
-	    $cv->begin;
-	    http_get $url, 
-	    headers => {
-		"user-agent" => "Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefox/17.0",
-		"Accept-Encoding" => "gzip, deflate",
-		'Accept-Language' => "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3",
-		'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	    },
-
-	    sub {
-		my ( $body, $hdr ) = @_;
-		
-		my $header = HTTP::Headers->new('content-encoding' => "gzip, deflate", 'content-type' => 'text/html');
-		my $mess = HTTP::Message->new( $header, $body );
-		my $content = $mess->decoded_content(charset => 'gb2312');
-		my $item = $self->parse($content);
-
-		$item->sku($sku);
-		$item->url($url);
-
-		debug_item($item);
-		
-		$self->add_item($item);
-		$cv->end;
-	    }
-	}
-	$cv->recv;
-    }
 }
 
 sub parse {
-    my $self    = shift;
-    my $content = shift;
+    
+}
 
-    my $item = Asthma::Item->new();
+sub fprice {
+    my $self = shift;
+    my $sku  = shift;
+    return unless $sku;
+    my $price_url = "http://jprice.360buy.com/price/np" . $sku . "-TRANSACTION-J.html";
 
-    if ( $content ) {
-	my $sku_tree = HTML::TreeBuilder->new_from_content($content);
-
-	if ( $sku_tree->look_down(_tag => 'h1') ) {
-	    $item->title($sku_tree->look_down(_tag => 'h1')->as_trimmed_text);
-	}
-        
-        if ( $sku_tree->look_down('id', 'product-intro') ) {
-            if ( my $price_image = $sku_tree->look_down('id', 'product-intro')->look_down("class", "p-price") ) {
-                if ( $price_image->look_down(_tag => "img") ) {
-                    my $price_url = $price_image->look_down(_tag => "img")->attr("src");
-                    if ( my $price = $self->get_price($price_url) ) {
-                        $item->price($price);
-                    }
-                }
-            }
-        }
-
-	if ( $sku_tree->look_down('id', 'spec-n1') ) {
-            if ( my $img = $sku_tree->look_down('id', 'spec-n1')->look_down(_tag => 'img') ) {
-                if ( my $image_url = $img->attr("src") ) {
-                    $item->image_url($image_url);
-                }
-            }
-        }
-        
-	$sku_tree->delete;
+    my $pres = $self->ua->get($price_url);
+    my $pcon = $pres->decoded_content;
+    
+    if ( $pcon =~ m/"jdPrice":\{.*?"amount"\s*:(.*?),/is ) {
+        return $1;
     }
 
-    return $item;
+    return;
 }
 
 __PACKAGE__->meta->make_immutable;
 
 1;
+
 
