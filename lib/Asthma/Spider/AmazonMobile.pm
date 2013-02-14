8b6139cf9285cc7de54da8ade037bbad4fec3bf1
Xiaonan Song <4king-gruuby@163.com>
2013-02-14 16:09:25 +0800

    fix undef content

--- lib/Asthma/Spider/AmazonMobile.pm
+++ lib/Asthma/Spider/AmazonMobile.pm
@@ -68,61 +68,60 @@ sub find {
     my $resp = shift;
     return unless $resp;
 
-    my $content = $resp->decoded_content;
+    if ( my $content = $resp->decoded_content ) {
+	my $tree = HTML::TreeBuilder->new_from_content($content);
+	
+	debug("process: " . $resp->request->uri->as_string);
 
-    my $tree = HTML::TreeBuilder->new_from_content($content);
-    
-    debug("process: " . $resp->request->uri->as_string);
-
-    if ( $tree->look_down('id', 'pagnNextLink') ) {
-	my $page_url = $tree->look_down('id', 'pagnNextLink')->attr('href');
-	$page_url = URI->new_abs($page_url, $resp->base)->as_string;
-	debug("get next page_url: $page_url");
-	push @{$self->urls}, $page_url;
-    }
-
-    my @skus;
-    if ( my @sku_divs = $tree->look_down('id', qr/result_\d+/) ) {
-	foreach my $div ( @sku_divs ) {
-	    push @skus, $div->attr('name');
+	if ( $tree->look_down('id', 'pagnNextLink') ) {
+	    my $page_url = $tree->look_down('id', 'pagnNextLink')->attr('href');
+	    $page_url = URI->new_abs($page_url, $resp->base)->as_string;
+	    debug("get next page_url: $page_url");
+	    push @{$self->urls}, $page_url;
 	}
-    }
-    
-    $tree->delete;
-
-    my $cv = AnyEvent->condvar;
-
-    foreach my $sku ( @skus ) {
-	next unless $sku;
-        my $url = "http://www.amazon.cn/dp/$sku";
-	$cv->begin;
-
-	http_get $url, 
-	headers => {
-	    "user-agent" => "Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefox/17.0",
-	    "Accept-Encoding" => "gzip, deflate",
-	    'Accept-Language' => "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3",
-	    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	},
-
-	sub {
-	    my ( $body, $hdr ) = @_;
-	    
-	    my $header = HTTP::Headers->new('content-encoding' => "gzip, deflate", 'content-type' => 'text/html');
-	    my $mess = HTTP::Message->new( $header, $body );
-	    my $content = $mess->decoded_content;
-	    my $item = $self->parse($content);
-
-	    $item->sku($sku);
-	    $item->url($url);
-
-	    debug_item($item);
-	    
-	    $self->add_item($item);
-	    $cv->end;
+	my @skus;
+	if ( my @sku_divs = $tree->look_down('id', qr/result_\d+/) ) {
+	    foreach my $div ( @sku_divs ) {
+		push @skus, $div->attr('name');
+	    }
 	}
+	
+	$tree->delete;
+
+	my $cv = AnyEvent->condvar;
+
+	foreach my $sku ( @skus ) {
+	    next unless $sku;
+	    my $url = "http://www.amazon.cn/dp/$sku";
+	    $cv->begin;
+
+	    http_get $url, 
+	    headers => {
+		"user-agent" => "Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefox/17.0",
+		"Accept-Encoding" => "gzip, deflate",
+		'Accept-Language' => "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3",
+		'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	    },
+
+	    sub {
+		my ( $body, $hdr ) = @_;
+		
+		my $header = HTTP::Headers->new('content-encoding' => "gzip, deflate", 'content-type' => 'text/html');
+		my $mess = HTTP::Message->new( $header, $body );
+		my $content = $mess->decoded_content;
+		my $item = $self->parse($content);
+
+		$item->sku($sku);
+		$item->url($url);
+
+		debug_item($item);
+		
+		$self->add_item($item);
+		$cv->end;
+	    }
+	}
+	$cv->recv;
     }
-    $cv->recv;
 }
 
 sub parse {
