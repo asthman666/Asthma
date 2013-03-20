fb173e55bbacf3106632ac86dc9dc0da60f36691
xiaonan song <4king-gruuby@163.com>
2013-03-20 10:34:37 +0800

    add day option

--- clean_solr.pl
+++ clean_solr.pl
@@ -5,9 +5,22 @@ use FindBin qw/$Bin/;
 use lib "$Bin/lib";
 use Asthma::Solr;
 use Asthma::Debug;
+use Getopt::Long;
+
+my $d;
+GetOptions(
+    "d|day=i" => \$d,    
+) or die "error parsing opt";
+
+my $delete_query;
+if ( $d ) {
+    $delete_query = "dt_created:[* TO NOW/DAY-$d" . ($d == 1 ? "DAY" : "DAYS") . "]";
+} else {
+    $delete_query = "dt_created:[* TO NOW/DAY]";
+}
 
 my $solr = Asthma::Solr->new();
-my $str = "<delete><query>dt_created:[* TO NOW/DAY]</query></delete>";
+my $str = "<delete><query>$delete_query</query></delete>";
 debug $str;
 $solr->update($str);
 
