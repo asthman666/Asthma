73af423adb081cdc03973f4f5223c88ff5a36e40
xiaonan song <4king-gruuby@163.com>
2013-04-11 07:14:31 +0800

    move header to UA package

--- lib/Asthma/UA.pm
+++ lib/Asthma/UA.pm
@@ -10,6 +10,16 @@ has 'ua_args'  => (is => 'rw', isa => 'HashRef', default => sub { {
     agent   => 'Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefox/17.0',
 } });
 
+has 'headers' => (is => 'rw', isa => 'HashRef', 
+		  default => sub { 
+		      {
+			  "user-agent" => "Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefox/17.0",
+			  "Accept-Encoding" => "gzip, deflate",
+			  'Accept-Language' => "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3",
+			  'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+		      } 
+		  });
+
 has 'ua' => (
     is => 'rw',
     lazy_build => 1
@@ -27,3 +37,4 @@ sub _build_ua {
 }
 
 1;
+
