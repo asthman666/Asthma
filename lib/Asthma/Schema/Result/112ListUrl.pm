eba92284ac0e8304c9635f4e8ce212ac7e47780f
xiaonan song <4king-gruuby@163.com>
2013-04-15 21:39:50 +0800

    add 1haodian data

---
+++ lib/Asthma/Schema/Result/112ListUrl.pm
@@ -0,0 +1,127 @@
+use utf8;
+package Asthma::Schema::Result::112ListUrl;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Asthma::Schema::Result::112ListUrl
+
+=cut
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use MooseX::MarkAsMethods autoclean => 1;
+extends 'DBIx::Class::Core';
+
+=head1 TABLE: C<112_list_url>
+
+=cut
+
+__PACKAGE__->table("112_list_url");
+
+=head1 ACCESSORS
+
+=head2 list_url_id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 dt_created
+
+  data_type: 'datetime'
+  datetime_undef_if_invalid: 1
+  default_value: '0000-00-00 00:00:00'
+  is_nullable: 0
+
+=head2 dt_updated
+
+  data_type: 'datetime'
+  datetime_undef_if_invalid: 1
+  default_value: '0000-00-00 00:00:00'
+  is_nullable: 0
+
+=head2 link
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 1024
+
+=head2 md5_link
+
+  data_type: 'char'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 22
+
+=cut
+
+__PACKAGE__->add_columns(
+  "list_url_id",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "dt_created",
+  {
+    data_type => "datetime",
+    datetime_undef_if_invalid => 1,
+    default_value => "0000-00-00 00:00:00",
+    is_nullable => 0,
+  },
+  "dt_updated",
+  {
+    data_type => "datetime",
+    datetime_undef_if_invalid => 1,
+    default_value => "0000-00-00 00:00:00",
+    is_nullable => 0,
+  },
+  "link",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 1024 },
+  "md5_link",
+  { data_type => "char", default_value => "", is_nullable => 0, size => 22 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</list_url_id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("list_url_id");
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<md5_link>
+
+=over 4
+
+=item * L</md5_link>
+
+=back
+
+=cut
+
+__PACKAGE__->add_unique_constraint("md5_link", ["md5_link"]);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07017 @ 2013-04-15 14:50:20
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:PrKhewdr5PjzK3qRuH+6Zw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->meta->make_immutable;
+1;
---
+++ lib/Asthma/Spider/1HaoDian.pm
@@ -0,0 +1,139 @@
+package Asthma::Spider::1HaoDian;
+use Moose;
+extends 'Asthma::Spider';
+
+use utf8;
+use Asthma::Item;
+use Asthma::Debug;
+use Data::Dumper;
+use Coro;
+use AnyEvent::HTTP;
+use HTTP::Headers;
+use HTTP::Message;
+use HTML::TreeBuilder;
+
+sub BUILD {
+    my $self = shift;
+    $self->site_id(112);
+}
+
+sub run {
+    my $self = shift;
+
+    my $page = 1;
+    my $run = 1;
+    while ( $run ) {
+        last unless $self->parse_item_urls($page++);
+    }
+}
+
+sub parse_item_urls {
+    my ($self, $page) = @_;
+    my $rows = 100;
+    debug("page: $page, rows: $rows");
+
+    my $urls = [$self->storage->mysql->resultset($self->site_id . 'ItemUrl')->search(
+		    undef,
+		    {
+			page => $page,
+			rows => $rows,
+		    })];
+
+    if ( @$urls ) {
+        my $sem = Coro::Semaphore->new(100);
+        my @coros;
+
+        foreach my $url_object ( @$urls ) {
+	    my $url = $url_object->link;
+
+            push @coros,
+            async {
+                my $guard = $sem->guard;
+
+                http_get $url,
+                headers => $self->headers,
+                Coro::rouse_cb;
+
+                my ($body, $hdr) = Coro::rouse_wait;
+                
+                my $header = HTTP::Headers->new('content-encoding' => 'gzip, deflate', 'content-type' => 'text/html');
+                #my $header = HTTP::Headers->new('content-type' => 'text/html');
+                my $mess = HTTP::Message->new( $header, $body );
+
+                if ( my $content = $mess->decoded_content() ) {
+                    my $sku_tree = HTML::TreeBuilder->new_from_content($content);
+
+                    my $item = Asthma::Item->new();
+
+                    if ( my $h2 = $sku_tree->look_down('id', 'productMainName') ) {
+                        if ( my @titles = $h2->content_list ) {
+                            $item->title($titles[0]);
+                        }
+                    }
+                    
+                    if ( $url =~ m{(?:item|product)/(\d+)} ) {
+                        my $sku = $1;
+                        $item->sku($sku);
+                        $item->url($url);
+                        $self->set_stock($item);
+                    }
+                    
+                    debug_item($item);
+                    $self->add_item($item);
+                }
+            }
+        }
+
+        $_->join foreach ( @coros );
+
+        return 1;
+    } else {
+        return 0;
+    }    
+
+}
+
+sub set_stock {
+    my $self = shift;
+    my $item = shift;
+
+    my $sku  = $item->sku;
+    my $url  = $item->url;
+
+    my $stock_url;
+
+    if ( $url =~ m{1mall\.com} ) {
+        $stock_url = "http://busystock.i.1mall.com/restful/detail?mcsite=1&provinceId=26&pmId=$sku";
+    } else {
+        $stock_url = "http://busystock.i.yihaodian.com/restful/detail?mcsite=1&provinceId=26&pmId=$sku";
+    }
+
+    my $resp = $self->ua->get($stock_url);
+    my $stock_content = $resp->decoded_content;
+
+    debug("url:$url");
+    debug("stock:$stock_url");
+    debug $stock_content;
+
+    use JSON;
+    my $ref = decode_json($stock_content);
+
+    #debug Dumper $ref;
+
+    if ( $ref->{currentPrice} ) {
+	$item->price($ref->{currentPrice});
+    }
+
+    if ( $ref->{currentStockNum} <= 0 ) {
+	$item->available('out of stock');
+    }
+}
+
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+
+
+
